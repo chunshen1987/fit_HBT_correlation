0f7d502afe8fe0fcdb73306ba38bffdfa15b208d
Chun Shen <chunshen1987@gmail.com>
2014-12-18 10:59:51 -0500

    move the results files to the results folder automatically

--- fit.sh
+++ fit.sh
@@ -6,4 +6,5 @@ for ii in `ls $path`
 do 
     ./fit_HBT.e $path/$ii
     awk {'print $1, $2, $4, $6, $8, $10, $16'} HBT_radii_fit_mode_1.dat > HBT_radii_kt_`echo $ii | cut -f 4 -d _ | cut -f 1 -d d`dat
+    mv HBT_radii_kt_* $path
 done
